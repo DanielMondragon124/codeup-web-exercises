c1cc8f6e01b126a833b36d368e9362a63f27385f
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-05-23 10:51:42 -0500

    update

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,11 +2,12 @@
 
 $(function() {
 
-    var contents = $('#SF90').html();
+    $('li').css('font-size','20px');
+    $('h1, p,li').css('background-color', 'yellow')
+    var contents = $('h1').html();
     alert(contents);
-
-    $('.codeup').css('border', '1px solid red');
-    // $('*').css('border', '1px solid #F00');
+    //
+    // $('.codeup').css('border', '1px solid red');
 
 
     // alert( 'The DOM has finished loading!' );
