642c79e43609c26e1f5f891d86b7bdf9ee038ff4
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-05-23 11:34:21 -0500

    update

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,10 +2,28 @@
 
 $(function() {
 
-    $('li').css('font-size','20px');
-    $('h1, p,li').css('background-color', 'yellow')
-    var contents = $('h1').html();
-    alert(contents);
+    $('h1').click(function() {
+        $(this).css('background-color', 'yellow');
+    });
+
+    $('p').dblclick(function(e){
+        $(this).css('font-size','18px');
+    });
+
+    $('li').hover(
+        function() {
+            $(this).css('color','red');
+        },
+        function() {
+            $(this).css('color','black');
+        }
+    )
+
+
+    // $('li').css('font-size','20px');
+    // $('h1, p,li').css('background-color', 'yellow')
+    // var contents = $('h1').html();
+    // alert(contents);
     //
     // $('.codeup').css('border', '1px solid red');
 
